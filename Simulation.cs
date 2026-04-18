fac25d428d5934f9bb001ec756be846afc095e47
luciawales <54911197+oolool4445@users.noreply.github.com>
2026-04-18 20:30:45 +0700

    Create Simulation.cs

---
+++ Simulation.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ScoriaProject
+{
+    public class Simulation
+    {
+        public void Start()
+        {
+            Console.WriteLine("Scoria formation simulation started.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Simulation ended.");
+        }
+    }
+}
