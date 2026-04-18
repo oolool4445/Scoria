b744b89963d16634875c076da7956c4966875541
luciawales <54911197+oolool4445@users.noreply.github.com>
2026-04-18 20:30:58 +0700

    Create Utils.cs

---
+++ Utils.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ScoriaProject
+{
+    public static class Utils
+    {
+        public static void Divider()
+        {
+            Console.WriteLine("================================");
+        }
+    }
+}
